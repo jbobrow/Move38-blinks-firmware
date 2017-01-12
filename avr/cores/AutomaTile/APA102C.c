e1585c5d32f1b8ea4ab78416e3ad75ebce2b549b
Josh Levine <github@junk.josh.com>
2017-01-12 16:53:08 -0500

    Consolidate bit and byte operations into a bitmask walker

--- avr/cores/AutomaTile/APA102C.c
+++ avr/cores/AutomaTile/APA102C.c
@@ -11,18 +11,10 @@
 
 #include "Pins.h"
 
-#define set(port,pin) (port |= pin) // set port pin
-#define clear(port,pin) (port &= (~pin)) // clear port pin
-#define bit_val(byte,bit) (byte & (1 << bit)) // test for bit set
-
-uint8_t portSet = 0;
 void setPort(volatile uint8_t* port){
-	portSet = 1;
-	SPI_PORT = port;
 }
 
 //bit bangs an SPI signal to the specified pins of the given data
-static void sendBit( uint8_t data){
     
     uint8_t bitmask = 0b10000000;
     
@@ -45,23 +37,9 @@ static void sendBit( uint8_t data){
     }
  }    
  
-//bit bangs an SPI signal to the specified pins of the given data
-void sendByte(uint8_t data){
-	sendBit( bit_val(data,7));
-	sendBit( bit_val(data,6));
-	sendBit( bit_val(data,5));
-	sendBit( bit_val(data,4));
-	sendBit( bit_val(data,3));
-	sendBit( bit_val(data,2));
-	sendBit( bit_val(data,1));
-	sendBit( bit_val(data,0));
-}
 //bit bangs an SPI signal to the specified pins that generates the specified color 
 //	formatted for the APA102, provided as a byte array of R,G,B
 void sendColor(uint8_t clkPin, uint8_t datPin,const rgb c){
-	if(!portSet){
-		return;
-	}
 	//Start Frame
 	sendByte( 0x00);
 	sendByte( 0x00);
