9df01a7c8e4d95085b755ed6fb1b6ceada65abfd
jbobrow <jbobrow@gmail.com>
2017-04-24 08:51:05 -0400

    updated to have state assciated with color. This allows the test to spread its color w/ color gossip tiles

--- examples/buttonMultipleClickDetections/buttonMultipleClickDetections.ino
+++ examples/buttonMultipleClickDetections/buttonMultipleClickDetections.ino
@@ -1,17 +1,20 @@
 /*
  *  Multiple Click Detection
- *  
+ *
  *  Demonstrates 3 functions
  *  1) buttonClicked() - which triggers after a button has been clicked once
  *  2) buttonDoubleClicked() - which triggers after a button has been clicked twice
  *  3) buttonTripleClicked() - which triggers after a button has been clicked 3 times
- *  
+ *
+ *  Note: states to communicate colors to neighboring tiles are chosen to match
+ *        the color gossip set of colors: red(1), cyan(2), magenta(3), yellow(4),...
+ *
  *  by Jonathan Bobrow & Luis Rodriguez
  *  01.19.2017
  */
- 
+
 void setup() {
-   setColorRGB(100, 0, 100);  // magenta until clicked   
+   setColorRGB(100, 0, 100);  // magenta until clicked
 }
 
 void loop() {
@@ -19,14 +22,17 @@ void loop() {
 
 void buttonClicked(){
   setColorRGB(255, 0, 0);  // red on single click
+  setState(1);             // communicates to another tile that it is red
 }
 
 void buttonDoubleClicked(){
   setColorRGB(0, 255, 0);  // green on double click
+  setState(7);             // communicates to another tile that it is green
 }
 
 void buttonTripleClicked(){
   setColorRGB(0, 0, 255); // blue on triple click
+  setState(5);            // communicates to another tile that it is blue
 }
 
 void buttonPressed() {
